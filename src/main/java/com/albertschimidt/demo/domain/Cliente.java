fe8ce07fc266efcbe13295e08a250c843fa60203
albertschimidt <albertschymydt@gmail.com>
2021-11-04 22:59:43 -0300

    subindo arquivos

---
+++ src/main/java/com/albertschimidt/demo/domain/Cliente.java
@@ -0,0 +1,7 @@
+package com.albertschimidt.demo.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Cliente {
+}
---
+++ src/main/java/com/albertschimidt/demo/repository/ClienteRepository.java
@@ -0,0 +1,4 @@
+package com.albertschimidt.demo.repository;
+
+public interface ClienteRepository extends org.springframework.data.repository.CrudRepository<com.albertschimidt.demo.domain.Cliente, java.lang.Long> {
+}
